8f3b6b31d13f791a08711286d177bd914b928f17
prabhatKrMishra <mprabhat774@gmail.com>
2025-05-12 06:44:37 +0000

    setup: Update kernel build script
    Signed-off-by: prabhatKrMishra <mprabhat774@gmail.com>

--- setup/build_kernel.sh
+++ setup/build_kernel.sh
@@ -3,8 +3,9 @@
 ROOT_PATH=$(pwd)
 BUILD_DIRECTORY=$ROOT_PATH/kernel
 SOURCE_DIRECTORY=$ROOT_PATH/kernel-5.10
-OUTPUT_DIRECTORY=../out
-MODULES_DIRECTORY=$OUTPUT_DIRECTORY/modules
+OUTPUT_DIRECTORY=out
+OUTPUT_DIRECTORY_PATH=$ROOT_PATH/out
+MODULES_DIRECTORY=$OUTPUT_DIRECTORY_PATH/modules
 VENDOR_BOOT_MODULES_LIST=$SOURCE_DIRECTORY/setup/moduleslist/vendor_boot/"modules.list"
 VENDOR_DLKM_MODULES_LIST=$SOURCE_DIRECTORY/setup/moduleslist/vendor_dlkm/"modules.list"
 
@@ -13,8 +14,8 @@ handle_arguments() {
         --new)
             SKIP_MRPROPER=0
             SKIP_DEFCONFIG=0
-            rm -rf $OUTPUT_DIRECTORY
-            mkdir -p $OUTPUT_DIRECTORY
+            rm -rf $OUTPUT_DIRECTORY_PATH
+            mkdir -p $OUTPUT_DIRECTORY_PATH
             ;;
         --rebuild)
             SKIP_MRPROPER=1
@@ -34,18 +35,18 @@ handle_arguments() {
 
 handle_arguments "$1"
 
-rm -rf $OUTPUT_DIRECTORY/modules
+rm -rf $OUTPUT_DIRECTORY_PATH/modules
 
 cd $SOURCE_DIRECTORY ;
-python scripts/gen_build_config.py --kernel-defconfig gta9_00_defconfig --kernel-defconfig-overlays "entry_level.config ot11.config ot11_data.config ot11_wifi.config" -m user -o $OUTPUT_DIRECTORY/build.config
+python scripts/gen_build_config.py --kernel-defconfig gta9_00_defconfig --kernel-defconfig-overlays "entry_level.config ot11.config ot11_data.config ot11_wifi.config" -m user -o ../$OUTPUT_DIRECTORY/build.config
 
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-android-
 export CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
-export OUT_DIR=$OUTPUT_DIRECTORY
-export DIST_DIR=$OUTPUT_DIRECTORY
-export BUILD_CONFIG=$OUTPUT_DIRECTORY/build.config
-export KERNEL_BUILD_MODE='user'
+export OUT_DIR=../$OUTPUT_DIRECTORY
+export DIST_DIR=../$OUTPUT_DIRECTORY
+export BUILD_CONFIG=../$OUTPUT_DIRECTORY/build.config
+export KERNEL_BUILD_MODE=user
 
 if [ "$SKIP_MRPROPER" -eq 1 ]; then
     export SKIP_MRPROPER=1
@@ -56,14 +57,14 @@ if [ "$SKIP_DEFCONFIG" -eq 1 ]; then
 fi
 
 cd $BUILD_DIRECTORY
-./build/build.sh -j$(($(nproc) - 1)) HOSTCC='clang' HOSTCXX='clang++' HOSTLD='ld.lld' CC='clang' CXX='clang++' LD='ld.lld' LLVM='clang' AR='llvm-ar' NM='llvm-nm' OBJCOPY='llvm-objcopy' OBJDUMP='llvm-objdump' OBJSIZE='llvm-size' READELF='llvm-readelf' STRIP='llvm-strip' LLVM=1 LLVM_IAS=1
+./build/build.sh LLVM_IAS=1 -j$(($(nproc) - 1))
 
 copy_modules() {
     echo "========================================================"
     echo " Proceeding with copying modules."
     rm -rf "$MODULES_DIRECTORY"
     mkdir "$MODULES_DIRECTORY"
-    mv $OUTPUT_DIRECTORY/*.ko "$MODULES_DIRECTORY"
+    mv $OUTPUT_DIRECTORY_PATH/*.ko "$MODULES_DIRECTORY"
 }
 
 rename_modules() {
@@ -78,7 +79,7 @@ select_modules_vendor_boot() {
 	  echo "Modules does not exist in $COMPILED_MODULES_DIR"
 	else
 	  COMPILED_MODULES_DIR=$MODULES_DIRECTORY
-	  DEST_DIR=$OUTPUT_DIRECTORY/vendor_boot_modules
+	  DEST_DIR=$OUTPUT_DIRECTORY_PATH/vendor_boot_modules
 
 	  rm -rf "$DEST_DIR"
 	  mkdir -p "$DEST_DIR"
@@ -99,7 +100,7 @@ select_modules_vendor_dlkm() {
 	  echo "Modules does not exist in $COMPILED_MODULES_DIR"
 	else
 	  COMPILED_MODULES_DIR=$MODULES_DIRECTORY
-	  DEST_DIR=$OUTPUT_DIRECTORY/vendor_dlkm_modules
+	  DEST_DIR=$OUTPUT_DIRECTORY_PATH/vendor_dlkm_modules
 
 	  rm -rf "$DEST_DIR"
 	  mkdir -p "$DEST_DIR"
@@ -117,16 +118,16 @@ select_modules_vendor_dlkm() {
 
 copy_binaries() {
 	echo " Copying kernel binaries"
-	KERNEL_BINARY_DIR=$OUTPUT_DIRECTORY/kernel-5.10/arch/arm64/boot
-	KERNEL_DTB_DIR=$OUTPUT_DIRECTORY/kernel-5.10/arch/arm64/boot/dts/mediatek
+	KERNEL_BINARY_DIR=$OUTPUT_DIRECTORY_PATH/kernel-5.10/arch/arm64/boot
+	KERNEL_DTB_DIR=$OUTPUT_DIRECTORY_PATH/kernel-5.10/arch/arm64/boot/dts/mediatek
 
-	cp "$KERNEL_BINARY_DIR/Image.gz" "$OUTPUT_DIRECTORY"
-	cp "$KERNEL_DTB_DIR/mt6789.dtb" "$OUTPUT_DIRECTORY/dtb"
-	cp "$KERNEL_DTB_DIR/mt8781_gta9_eur_open_00.dtbo" "$OUTPUT_DIRECTORY/dtbo"
+	cp "$KERNEL_BINARY_DIR/Image.gz" "$OUTPUT_DIRECTORY_PATH"
+	cp "$KERNEL_DTB_DIR/mt6789.dtb" "$OUTPUT_DIRECTORY_PATH/dtb"
+	cp "$KERNEL_DTB_DIR/mt8781_gta9_eur_open_00.dtbo" "$OUTPUT_DIRECTORY_PATH/dtbo"
 }
 
 # Check if 'arch/arm64/boot/Image.gz' exists
-if [ -f $OUTPUT_DIRECTORY/kernel-5.10/arch/arm64/boot/Image.gz ]; then
+if [ -f $OUTPUT_DIRECTORY_PATH/kernel-5.10/arch/arm64/boot/Image.gz ]; then
     echo ''
     echo " Kernel build successful! "
     echo ''
